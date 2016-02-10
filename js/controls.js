23391fd622511af2770287846a3517a96bac0c1e
techmsi <Sophie.Idromenos@nymag.com>
2016-02-10 13:59:45 -0500

    Added play/pause reader functionality

--- js/controls.js
+++ js/controls.js
@@ -1,11 +1,40 @@
 var _ = require('lodash');
+var $ = require('jquery');
 var remote = require('remote');
 var dialog = remote.require('dialog');
 var fs = require('fs');
+var editor = document.getElementById('editor');
+var wordsPerMinute = 100;
 
 module.exports.getWords = function (){
-  var text = document.getElementById('editor').innerText;
+  var text = editor.innerText;
   return _.words(text);
+}
+
+module.exports.readWords = function (isPaused){
+  var words = this.getWords(),
+      $reader = $('#reader'),
+      total = words.length,
+      lastWordRead = $reader.attr('data-index'),
+      current = (lastWordRead) ? lastWordRead : 0;
+
+    (function cycle() {
+        console.log('current - %s, %s', current, words[current]);
+
+        $reader
+        // .removeClass('paused')
+        .attr('data-index', current)
+        .animate({ opacity:1.0 }, 400)
+        .text(words[current])
+        .delay(wordsPerMinute * 10)
+        .animate({ opacity:0 }, cycle);
+
+        if (isPaused) {
+          $reader.stop(true, true);
+        }
+        current = ++current % total;
+
+  })();
 }
 
 module.exports.openFile = function () {
@@ -19,7 +48,7 @@ module.exports.openFile = function () {
   var fileName = fileNames[0];
 
   fs.readFile(fileName, 'utf-8', function (err, data) {
-    document.getElementById('editor').innerText = data;
+    editor.innerText = data;
   });
  });
 }
