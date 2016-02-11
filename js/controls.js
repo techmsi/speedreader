3573d513b7c3ed7791c65ee956eda32aa8af4e13
techmsi <Sophie.Idromenos@nymag.com>
2016-02-11 13:15:28 -0500

    Improved play/pause reader functionality

--- js/controls.js
+++ js/controls.js
@@ -5,21 +5,35 @@ var dialog = remote.require('dialog');
 var fs = require('fs');
 var editor = document.getElementById('editor');
 var wordsPerMinute = 100;
+this.current = 0;
+this.words = '';
 
 module.exports.getWords = function (){
   var text = editor.innerText;
   return _.words(text);
 }
 
+module.exports.setWords = function (){
+  var text = this.getWords();
+  document.querySelector('.word').innerText = text[0];
+  this.words = text;
+}
+
 module.exports.readWords = function (isPaused){
-  var words = this.getWords(),
+
+  var self = this,
+      words = self.words,
+      current = self.current,
       $reader = $('#reader'),
       total = words.length,
-      lastWordRead = $reader.attr('data-index'),
-      current = (lastWordRead) ? lastWordRead : 0;
+      lastWordRead = $reader.attr('data-index');
 
+
+      if (current > 0) {
+        current = (lastWordRead) ? lastWordRead : 0;
+      }
     (function cycle() {
-        console.log('current - %s, %s', current, words[current]);
+        console.log('current - paused: %s, %s, %s', isPaused, current, words[current]);
 
         $reader
         .attr('data-index', current)
@@ -30,6 +44,7 @@ module.exports.readWords = function (isPaused){
 
         if (isPaused) {
           $reader.stop(true, true);
+          console.log('%o', words);
         }
         current = ++current % total;
 
@@ -37,6 +52,7 @@ module.exports.readWords = function (isPaused){
 }
 
 module.exports.openFile = function () {
+ var self = this;
 
  dialog.showOpenDialog({ filters: [
    { name: 'text', extensions: ['txt'] }
@@ -47,7 +63,10 @@ module.exports.openFile = function () {
   var fileName = fileNames[0];
 
   fs.readFile(fileName, 'utf-8', function (err, data) {
-    editor.innerText = data;
+    editor.innerHTML = data;
+    // Stop all animation then restart
+    self.current = 0;
+    self.setWords();
   });
  });
 }
